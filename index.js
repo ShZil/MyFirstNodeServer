f1fff549aa6fa2d974bfa075e4ff32068284d781
ShZil <shzil333@gmail.com>
2021-03-04 00:52:18 +0200

    Solved the connection, added appliable css file!
    Now I can actually style the response from java docs!!!

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 // https://www.w3schools.com/nodejs/nodejs_url.asp
 
 const http = require('http');
+const fs = require('fs');
 const jsdom = require("jsdom");
+const url = require("url");
 const { JSDOM } = jsdom;
 
 // http://localhost:3000
@@ -23,11 +25,10 @@ const htmlParserOptions = {
 const webpage = {
   PREContent: [
     "Hi Dude!",
-    "You used this url ending:",
-    "$url",
+    "You used this url:",
+    "http://localhost:3000$url",
     "Hello World!",
-    "This is "+options.host,
-    "$google"
+    "This is "+options.host
   ],
   lineSeperator: '<br>',
   continuation: "...",
@@ -36,13 +37,15 @@ const webpage = {
     for (var i = 0; i < contentArr.length; i++) {
       acc += contentArr[i]
       .replaceAll("$url", request.url)
-      .replaceAll("$google", deepHTMLParser(options.response));
       //.replaceAll("$reqAll", Object.getOwnPropertyNames(request).join(", "))
       //.replaceAll("$reqVal", deepSafeObjectReader(request, 0))
-
       acc += webpage.lineSeperator;
     }
-    return acc + "</div>";
+    acc += "</div>";
+    acc += "<div id=\"response\">";
+    acc += options.response;
+    acc += "</div>";
+    return acc;
   },
   propertyIndent: 50,
   maxRuns: 3,
@@ -56,83 +59,92 @@ const webpage = {
   }
 };
 
-const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.writeHead(200,{"Content-Type" : "text/html"});
-  response.write(webpage.parseContent(webpage.PREContent, request));
-  response.end();
-  console.log("Ended Response.");
+const server = http.createServer(function(req, res){
+  // your normal server code
+  var path = url.parse(req.url).pathname;
+  switch (path){
+    case '/':
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(webpage.parseContent(webpage.PREContent, request));
+    res.end();
+    break;
+
+    default:
+    if (/\.(js|html|swf)$/.test(path)){
+      try {
+        var swf = path.substr(-4) === '.swf';
+        res.writeHead(200, {'Content-Type': swf ? 'application/x-shockwave-flash' : ('text/' + (path.substr(-3) === '.js' ? 'javascript' : 'html'))});
+        res.end();
+      } catch(e){
+        send404(res);
+      }
+      break;
+    }
+    else if (/\.(css)$/.test(path)){
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      res.write(fs.readFileSync(__dirname + "\\main.css", 'utf8'));
+      res.end();
+      break;
+    }
+
+    // send404(res);
+    break;
+  }
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});
+  });
 
-var request = require("request");
+  var request = require("request");
 
-var parseMyAwesomeHtml = function(html) {
+  var parseMyAwesomeHtml = function(html) {
     console.log("SUCCESS!");
     console.log(html);
     options.response = html;
-};
+  };
 
-request(options.host, function (error, response, body) {
+  request(options.host, function (error, response, body) {
     if (!error) {
-        parseMyAwesomeHtml(body);
+      parseMyAwesomeHtml(body);
     } else {
-        console.log(error);
+      console.log(error);
     }
-});
+  });
 
 
-const colorString = (str, clr) => clr ? `<b style=\"color: ${clr};\">${str}</b>` : str;
+  const colorString = (str, clr) => clr ? `<b style=\"color: ${clr};\">${str}</b>` : str;
 
-function deepSafeObjectReader(obj, runs) {
-        console.log(typeof obj + " " + runs);
-        var color = webpage.typeofColors[typeof obj];
-        if (obj === undefined) {
-          return colorString("undefined", color);
-        }
-        if (obj === null) {
-          return colorString("null", webpage.typeofColors[obj]);
-        }
-        if ((typeof obj) === "string") {
-          return colorString(`\"${obj}\"`, color);
-        }
-        if ((typeof obj) === "number") {
-          return colorString(`${obj}`, color);
-        }
-        if ((typeof obj) === "function") {
-          return colorString(`function ${obj.name}(${obj.length == 0 ? "" : obj.length})`, color);
-        }
-        if ((typeof obj) === "boolean") {
-          return colorString(`${obj.toString()}`, color);
-        }
-        if (runs >= webpage.maxRuns) {
-          return "<i>...</i>";
-        }
-        var str = "{<br>";
-        for (const [key, value] of Object.entries(obj)) {
-          str +=
-          `<b style=\"margin-left: ${webpage.propertyIndent * runs}px;\">` +
-          `${key}: ${deepSafeObjectReader(value, runs+1)}</b>,<br>`;
-        }
-        str += "}";
-        return str;
-      }
-
-function deepHTMLParser(options, text) {
-  if (options.plainText) {
-    return text.replaceAll("<", "&lt;");
+  function deepSafeObjectReader(obj, runs) {
+    console.log(typeof obj + " " + runs);
+    var color = webpage.typeofColors[typeof obj];
+    if (obj === undefined) {
+      return colorString("undefined", color);
+    }
+    if (obj === null) {
+      return colorString("null", webpage.typeofColors[obj]);
+    }
+    if ((typeof obj) === "string") {
+      return colorString(`\"${obj}\"`, color);
+    }
+    if ((typeof obj) === "number") {
+      return colorString(`${obj}`, color);
+    }
+    if ((typeof obj) === "function") {
+      return colorString(`function ${obj.name}(${obj.length == 0 ? "" : obj.length})`, color);
+    }
+    if ((typeof obj) === "boolean") {
+      return colorString(`${obj.toString()}`, color);
+    }
+    if (runs >= webpage.maxRuns) {
+      return "<i>...</i>";
+    }
+    var str = "{<br>";
+    for (const [key, value] of Object.entries(obj)) {
+      str +=
+      `<b style=\"margin-left: ${webpage.propertyIndent * runs}px;\">` +
+      `${key}: ${deepSafeObjectReader(value, runs+1)}</b>,<br>`;
+    }
+    str += "}";
+    return str;
   }
-  const dom = new JSDOM(text);
-  const { document } = dom.window;
-  if (options.disableScripts) {
-    document.querySelectorAll("script").forEach(element => {
-      element.innerHTML = "";
-      element.src = "";
-    });
-  }
-
-  return document.documentElement.outerHTML;
-}
